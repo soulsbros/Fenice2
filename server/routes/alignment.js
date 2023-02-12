5480a827141dd39d5faed7c0eea1fdc1df5e1d8c
Stefano Taillefert <stefano.taille@gmail.com>
2023-02-12 03:31:32 +0000

    Add old fenice integration

--- server/routes/alignment.js
+++ server/routes/alignment.js
@@ -54,7 +54,26 @@ router.get('/getCharactersByCampaignName/:name', async (req, res) => {
       }),
     );
 
-    res.status(200).send(chars.sort((a, b) => a.name.localeCompare(b.name)));
+    // For each character, get data from the old DB (SQL)
+    let result = [];
+    for (const character of chars) {
+      if (character.externalId !== null) {
+        const response = await fetch(
+          `https://lafenice.soulsbros.ch/actions/getCharacterData.php?id=${character.externalId}`,
+        );
+        try {
+          const body2 = await response.json();
+          result.push({ ...body2, ...character });
+        } catch (err) {
+          console.error(character.externalId, err);
+          result.push(character);
+        }
+      } else {
+        result.push(character);
+      }
+    }
+
+    res.status(200).send(result.sort((a, b) => a.name.localeCompare(b.name)));
   } catch (err) {
     console.error(err);
     res.status(500).send({ error: err.toString() });
