ec11953ab564ae3da4c4d65fd55b2210ac8bfc41
Andrea Brites Marto <andreabritesma@gmail.com>
2023-02-12 03:19:45 +0000

    Add alignment API

--- server/routes/alignment.js
+++ server/routes/alignment.js
@@ -1,84 +1,176 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const router = express.Router();
+const { ourMongo } = require('../middleware/mongoConn');
+const ObjectID = require('mongodb').ObjectID;
 
-/**
- * Get all campaigns (proxy for now)
- */
-router.get('/getCampaigns', async (req, res) => {
+router.get('/getAllCharacters', async (req, res) => {
   try {
-    const response = await fetch(`https://alignment.lafenice.soulsbros.ch/api/getCampaigns`);
-    const body = await response.json();
+    let db = await ourMongo('characters');
+    let cursor = db.find();
+    let chars = [];
+    await cursor.forEach((el) => chars.push(el));
 
-    res.status(200).send(body.sort((a, b) => a.name.localeCompare(b.name)));
+    res.status(200).send(chars.sort((a, b) => a.name.localeCompare(b.name)));
   } catch (err) {
     console.error(err);
-    res.status(500).send({});
+    res.status(500).send({ error: err.toString() });
+  }
+});
+
+router.get('/getCharactersByCampaign/:id', async (req, res) => {
+  try {
+    let db = await ourMongo('campaigns');
+    let campaignId = new ObjectID(req.params.id);
+    let campaign = await db.findOne({ _id: campaignId });
+
+    db = await ourMongo('characters');
+    let chars = [];
+    await Promise.all(
+      campaign.characters.map(async (charId) => {
+        let char = await db.findOne({ _id: new ObjectID(charId) });
+        chars.push(char);
+      }),
+    );
+
+    res.status(200).send(chars.sort((a, b) => a.name.localeCompare(b.name)));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: err.toString() });
   }
 });
 
 router.get('/getCharactersByCampaignName/:name', async (req, res) => {
   try {
-    // Get data from new DB (mongo)
-    const response = await fetch(
-      `https://alignment.lafenice.soulsbros.ch/api/getCharactersByCampaignName/${req.params.name}`,
+    let db = await ourMongo('campaigns');
+    let campaignName = req.params.name;
+    let campaign = await db.findOne({ name: campaignName });
+
+    db = await ourMongo('characters');
+    let chars = [];
+    await Promise.all(
+      campaign.characters.map(async (charId) => {
+        let char = await db.findOne({ _id: new ObjectID(charId) });
+        chars.push(char);
+      }),
     );
-    const body = await response.json();
 
-    // For each character, get data from the old DB (SQL)
-    let result = [];
-    for (const character of body) {
-      if (character.externalId !== null) {
-        const response = await fetch(
-          `https://lafenice.soulsbros.ch/actions/getCharacterData.php?id=${character.externalId}`,
-        );
-        try {
-          const body2 = await response.json();
-          result.push({ ...body2, ...character });
-        } catch (err) {
-          console.error(character.externalId, err);
-          result.push(character);
-        }
-      } else {
-        result.push(character);
-      }
-    }
-
-    res.status(200).send(result.sort((a, b) => a.name.localeCompare(b.name)));
+    res.status(200).send(chars.sort((a, b) => a.name.localeCompare(b.name)));
   } catch (err) {
     console.error(err);
-    res.status(500).send({});
+    res.status(500).send({ error: err.toString() });
   }
 });
 
-router.get('/getCharactersByCampaign/:name', async (req, res) => {
+router.get('/getCampaigns', async (req, res) => {
   try {
-    const response = await fetch(
-      `https://alignment.lafenice.soulsbros.ch/api/getCharactersByCampaign/${req.params.name}`,
-    );
-    const body = await response.json();
+    let db = await ourMongo('campaigns');
+    let cursor = db.find();
+    let campaigns = [];
+    await cursor.forEach((el) => campaigns.push(el));
 
-    res.status(200).send(body.sort((a, b) => a.name.localeCompare(b.name)));
+    res.status(200).send(campaigns.sort((a, b) => a.name.localeCompare(b.name)));
   } catch (err) {
     console.error(err);
-    res.status(500).send({});
+    res.status(500).send({ error: err.toString() });
+  }
+});
+
+router.get('/getCampaignById/:id', async (req, res) => {
+  try {
+    let db = await ourMongo('campaigns');
+    let campaignId = new ObjectID(req.params.id);
+    let campaign = await db.findOne({ _id: campaignId });
+
+    res.status(200).send(campaign);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: err.toString() });
+  }
+});
+
+router.post('/insertCharacter', async (req, res) => {
+  try {
+    let chars = req.body;
+    let db = await ourMongo('characters');
+    let result = await db.insertMany(chars);
+
+    if (result.insertedCount > 0) {
+      res.status(201).send({
+        msg: result.insertedCount + ' character(s) inserted successfully',
+      });
+    } else {
+      console.error('Error inserting docs ', result);
+      res.status(500).send({ error: 'Error inserting docs ' + result });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: err.toString() });
   }
 });
 
 router.post('/addAction', async (req, res) => {
   try {
-    await fetch(`https://alignment.lafenice.soulsbros.ch/api/addAction`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(req.body),
-    });
+    const charId = new ObjectID(req.body.charId);
+    let charAction = req.body.action;
+    let db = await ourMongo('characters');
 
-    res.status(200).send('ok');
+    let result = await db.updateOne(
+      { _id: charId },
+      {
+        $push: {
+          actionsHistory: charAction,
+        },
+      },
+    );
+
+    if (result && result.modifiedCount === 1) {
+      let value = await db.findOne({ _id: charId });
+      if (charAction.type === 'Good' || charAction.type === 'Evil') {
+        let newValue = value.goodEvilValue + charAction.value;
+        if (newValue < -100) {
+          newValue = -100;
+        } else if (newValue > 100) {
+          newValue = 100;
+        }
+        result = db.updateOne({ _id: charId }, { $set: { goodEvilValue: newValue } });
+      } else {
+        let newValue = value.lawfulChaoticValue + charAction.value;
+        if (newValue < -100) {
+          newValue = -100;
+        } else if (newValue > 100) {
+          newValue = 100;
+        }
+        result = db.updateOne({ _id: charId }, { $set: { lawfulChaoticValue: newValue } });
+      }
+
+      res.status(201).send({ msg: ' Updated successfully', result });
+    } else {
+      console.error('Error updating action ', result);
+      res.status(500).send({ error: 'Error updating action', result });
+    }
   } catch (err) {
     console.error(err);
-    res.status(500).send({});
+    res.status(500).send({ error: err.toString() });
+  }
+});
+
+router.post('/addCampaigns', async (req, res) => {
+  try {
+    let campaigns = req.body;
+    let db = await ourMongo('campaigns');
+    let result = await db.insertMany(campaigns);
+
+    if (result.insertedCount > 0) {
+      res.status(201).send({
+        msg: result.insertedCount + ' campaign(s) inserted successfully',
+      });
+    } else {
+      console.error('Error inserting docs ', result);
+      res.status(500).send({ error: 'Error inserting docs ' + result });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: err.toString() });
   }
 });
 
---
+++ server/routes/alignment_old.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const router = express.Router();
+
+/**
+ * Get all campaigns (proxy for now)
+ */
+router.get('/getCampaigns', async (req, res) => {
+  try {
+    const response = await fetch(`https://alignment.lafenice.soulsbros.ch/api/getCampaigns`);
+    const body = await response.json();
+
+    res.status(200).send(body.sort((a, b) => a.name.localeCompare(b.name)));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({});
+  }
+});
+
+router.get('/getCharactersByCampaignName/:name', async (req, res) => {
+  try {
+    // Get data from new DB (mongo)
+    const response = await fetch(
+      `https://alignment.lafenice.soulsbros.ch/api/getCharactersByCampaignName/${req.params.name}`,
+    );
+    const body = await response.json();
+
+    // For each character, get data from the old DB (SQL)
+    let result = [];
+    for (const character of body) {
+      if (character.externalId !== null) {
+        const response = await fetch(
+          `https://lafenice.soulsbros.ch/actions/getCharacterData.php?id=${character.externalId}`,
+        );
+        try {
+          const body2 = await response.json();
+          result.push({ ...body2, ...character });
+        } catch (err) {
+          console.error(character.externalId, err);
+          result.push(character);
+        }
+      } else {
+        result.push(character);
+      }
+    }
+
+    res.status(200).send(result.sort((a, b) => a.name.localeCompare(b.name)));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({});
+  }
+});
+
+router.get('/getCharactersByCampaign/:name', async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://alignment.lafenice.soulsbros.ch/api/getCharactersByCampaign/${req.params.name}`,
+    );
+    const body = await response.json();
+
+    res.status(200).send(body.sort((a, b) => a.name.localeCompare(b.name)));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({});
+  }
+});
+
+router.post('/addAction', async (req, res) => {
+  try {
+    await fetch(`https://alignment.lafenice.soulsbros.ch/api/addAction`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(req.body),
+    });
+
+    res.status(200).send('ok');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({});
+  }
+});
+
+module.exports = router;
