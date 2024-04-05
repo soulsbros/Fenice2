19888b79928f96e1979b50e40aba014cd34d34af
Stefano Taillefert <stefano.taille@gmail.com>
2024-04-05 19:33:24 +0200

    Add missing fields count to cards

--- src/components/characterCard.tsx
+++ src/components/characterCard.tsx
@@ -1,7 +1,8 @@
+import { getCampaigns } from "@/actions/characters";
 import defaultUser from "@/img/defaultUser.png";
 import { Character } from "@/types/API";
 import Link from "next/link";
-import { Compass, User } from "react-feather";
+import { AlertCircle, Compass, User, Users } from "react-feather";
 import ImageWithFallback from "./imageWithFallback";
 
 interface CharacterCardProps {
@@ -9,10 +10,32 @@ interface CharacterCardProps {
   showPlayer?: boolean;
 }
 
-export default function CharacterCard({
+const countEmptyFields = (character: Character) => {
+  const fieldsToCheck = [
+    "backstory",
+    "personality",
+    "gender",
+    "pronouns",
+    "orientation",
+  ] as (keyof Character)[];
+  let emptyFields = 0;
+
+  for (const field of fieldsToCheck) {
+    if (character[field] == "") {
+      ++emptyFields;
+    }
+  }
+  return emptyFields;
+};
+
+export default async function CharacterCard({
   character,
   showPlayer = true,
 }: Readonly<CharacterCardProps>) {
+  const emptyFields = countEmptyFields(character);
+  const campaign = await getCampaigns(undefined, { _id: character.campaignId });
+  const campaignName = campaign.data[0].name;
+
   return (
     <div className="character-card w-[300px] m-3 p-4 rounded-md shadow-md">
       <Link href={`/characters/${character._id}`}>
@@ -28,14 +51,28 @@ export default function CharacterCard({
         <p>
           {character.race} {character.class}
         </p>
-        <p className="flex mt-2">
+        <p className="flex mt-2" title="Alignment">
           <Compass />
           &nbsp; {character.actualAlignment}
         </p>
         {showPlayer ? (
-          <p className="flex mt-2">
+          <p className="flex mt-2" title="Player">
             <User />
             &nbsp; {character.player}
+          </p>
+        ) : (
+          <p className="flex mt-2" title="Campaign">
+            <Users />
+            &nbsp; {campaignName}
+          </p>
+        )}
+        {!showPlayer && emptyFields > 0 ? (
+          <p
+            className="flex mt-2 justify-end text-red-600"
+            title="Empty fields"
+          >
+            <AlertCircle />
+            &nbsp; <span>{emptyFields}</span>
           </p>
         ) : null}
       </Link>
