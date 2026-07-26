9e01cd4655ffb1bf4a94bb158a0c2bf31555f20b
Stefano Taillefert <stefano.taille@gmail.com>
2026-07-26 15:29:23 +0200

    Make pdf viewer bigger

--- src/app/documents/viewer/[document]/page.tsx
+++ src/app/documents/viewer/[document]/page.tsx
@@ -2,7 +2,6 @@ import { getSignedURL } from "@/actions/storage";
 import { editions } from "@/lib/skills";
 import { cleanDocTitle } from "@/lib/utils";
 import { Metadata } from "next";
-import Link from "next/link";
 
 interface Props {
   params: { document: string };
@@ -30,14 +29,12 @@ export default async function ViewerPage({ params }: Readonly<Props>) {
         {editions.find((e) => e.id === docMetadata.edition)?.name})
       </div>
 
-      <Link href={url} className="primary button" download>
-        Download
-      </Link>
-      <Link href={url} className="primary button" target="_blank">
-        Open in new tab
-      </Link>
-
-      <iframe src={url} width="100%" height="85%" title="PDF file"></iframe>
+      <iframe
+        src={url}
+        className="h-screen"
+        width="100%"
+        title="PDF file"
+      ></iframe>
     </>
   );
 }
