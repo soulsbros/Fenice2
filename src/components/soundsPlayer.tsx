b0af9bd095b0e7059f9d9de39ec0bad391449534
Stefano Taillefert <stefano.taille@gmail.com>
2026-09-07 01:29:06 +0200

    Add recordings page

--- src/components/soundsPlayer.tsx
+++ src/components/soundsPlayer.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { getSignedURL } from "@/actions/storage";
 import { cleanSoundTitle } from "@/lib/utils";
 import { useEffect, useState } from "react";
+import Button from "./button";
 
 interface Props {
   sounds: { name: string; URL: string }[];
@@ -33,6 +35,50 @@ export default function SoundsPlayer({ sounds }: Readonly<Props>) {
         ))}
       </div>
       <audio id="player" controls className="hidden" />
+    </>
+  );
+}
+
+interface RecordingsProps {
+  sounds: { name: string; folder: string; fullPath: string }[];
+}
+
+export function RecordingsPlayer({ sounds }: Readonly<RecordingsProps>) {
+  const [audio, setAudio] = useState<HTMLAudioElement>();
+  const [title, setTitle] = useState("");
+
+  async function playRecording(sound: {
+    name: string;
+    folder: string;
+    fullPath: string;
+  }) {
+    setTitle(sound.name);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    const url = await getSignedURL(sound.fullPath);
+    audio!.src = url;
+    audio!.play();
+  }
+
+  useEffect(() => {
+    setAudio(document.getElementById("recordingsPlayer")! as HTMLAudioElement);
+  }, []);
+
+  return (
+    <>
+      {title ? <p>Now playing: {title}</p> : null}
+      <audio
+        id="recordingsPlayer"
+        controls
+        className={`w-full mb-6 ${title ? "" : "hidden"}`}
+      />
+
+      {sounds.map((sound) => {
+        return (
+          <div key={sound.fullPath}>
+            <Button onClick={() => playRecording(sound)} label={sound.name} />
+          </div>
+        );
+      })}
     </>
   );
 }
